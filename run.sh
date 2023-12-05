0df34bbfa54c576aaf9ce0f1f6b08fa4da23287c
PRENTICED103 <Dean>
2023-12-05 10:14:16 +0000

    updated run.sh file

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
-echo "hello world"
-echo "merry christmas"
-echo "and a happy new year"
+echo "hello world" >1.text
+echo "merry christmas" >2.text
+echo "and a happy new year" >3.text
+
+zip archive.zip *.text
+
